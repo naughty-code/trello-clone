aba299a6315e5e77bfa348390d713185685edf2f
naughty-code <javier.murar@gmail.com>
2020-03-16 15:31:39 -0400

    Updates

---
+++ precache-manifest.8109a90895815025ff9b8896528fb92c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7347d58f2d2291d34d4eb535a1fb8b0",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "52baa86c03927e9a386c",
+    "url": "/trello-clone/static/css/main.5bb6d772.chunk.css"
+  },
+  {
+    "revision": "9b254238ab5df2d73bb7",
+    "url": "/trello-clone/static/js/2.b7aeb538.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/trello-clone/static/js/2.b7aeb538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52baa86c03927e9a386c",
+    "url": "/trello-clone/static/js/main.988eeda3.chunk.js"
+  },
+  {
+    "revision": "01d9cced60e505c9249c",
+    "url": "/trello-clone/static/js/runtime-main.17cd50a1.js"
+  }
+]);
